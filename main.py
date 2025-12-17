008c0a1f19db45dfa28e96f903470eff15f7cd32
Lysenko <lysenkoden@gmail.com>
2025-12-17 21:54:00 +0100

    a class has been created

---
+++ main.py
@@ -0,0 +1,28 @@
+	class Category:
+	    def __init__(self,name):
+	        self.name=name
+	        self.ledger=[]
+	    def deposit(self,amount,description=''):
+	        self.ledger.append({'amount': amount, 'description': description})
+	    def withdraw(self,amount,description=''):
+	        if self.check_funds(amount):
+	            self.ledger.append({'amount': amount*-1, 'description': description})
+	            return True
+	        else:
+	            return False
+	    def get_balance(self):
+	        balance=0
+	        for item in self.ledger:
+	            balance+=item['amount']
+	        return balance
+	    def transfer(self, amount, another_category):
+	        if self.withdraw(amount, f"Transfer to {another_category.name}"):
+	            another_category.deposit(amount, f"Transfer from {self.name}")
+	            return True
+	        else:
+	            return False
+	    def check_funds(self,amount):
+	        if self.get_balance()>=amount:
+	            return True
+	        else:
+            return False
